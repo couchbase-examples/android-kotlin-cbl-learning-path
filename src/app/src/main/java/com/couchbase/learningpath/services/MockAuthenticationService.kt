10af9e3591b088bf3c1f8d89f1011ddbd7f6ec48
Aaron LaBeau <alabeau@gmail.com>
2022-05-05 10:59:46 -0500

    updated docker support

--- src/app/src/main/java/com/couchbase/learningpath/services/MockAuthenticationService.kt
+++ src/app/src/main/java/com/couchbase/learningpath/services/MockAuthenticationService.kt
@@ -21,7 +21,7 @@ class MockAuthenticationService : AuthenticationService {
                 false
             }
         } else {
-            _user = User(username = username, password = password, team = "Santa Clara Team")
+            _user = User(username = username, password = password, team = "team2")
             return true
         }
     }
@@ -33,21 +33,21 @@ class MockAuthenticationService : AuthenticationService {
     init {
         //create mock users for testing the application
         //in a real app this would be provided by some kind of OAuth2 Service, etc
-        _mockUsers["demo@example.com"] = User("demo@example.com", "password", "Santa Clara Team")
-        _mockUsers["demo1@example.com"] = User("demo1@example.com", "password", "Santa Clara Team")
-        _mockUsers["demo2@example.com"] = User("demo2@example.com", "password", "London Team")
-        _mockUsers["demo3@example.com"] = User("demo3@example.com", "password", "London Team")
-        _mockUsers["demo4@example.com"] = User("demo4@example.com", "password", "Austin Team")
-        _mockUsers["demo5@example.com"] = User("demo5@example.com", "password", "Austin Team")
-        _mockUsers["demo6@example.com"] = User("demo6@example.com", "password", "San Francisco Team")
-        _mockUsers["demo7@example.com"] = User("demo7@example.com", "password", "San Francisco Team")
-        _mockUsers["demo8@example.com"] = User("demo8@example.com", "password", "Manchester Team")
-        _mockUsers["demo9@example.com"] = User("demo9@example.com", "password", "Manchester Team")
-        _mockUsers["demo10@example.com"] = User("demo10@example.com", "password", "Bangalore Team")
-        _mockUsers["demo11@example.com"] = User("demo11@example.com", "password", "Bangalore Team")
-        _mockUsers["demo12@example.com"] = User("demo12@example.com", "password", "Atlanta Team")
-        _mockUsers["demo13@example.com"] = User("demo13@example.com", "password", "Seattle Team")
-        _mockUsers["demo14@example.com"] = User("demo14@example.com", "password", "Kinshasa Team")
-        _mockUsers["demo15@example.com"] = User("demo15@example.com", "password", "Tokyo Team")
+        _mockUsers["demo@example.com"] = User("demo@example.com", "password", "team1")
+        _mockUsers["demo1@example.com"] = User("demo1@example.com", "password", "team1")
+        _mockUsers["demo2@example.com"] = User("demo2@example.com", "password", "team2")
+        _mockUsers["demo3@example.com"] = User("demo3@example.com", "password", "team2")
+        _mockUsers["demo4@example.com"] = User("demo4@example.com", "password", "team3")
+        _mockUsers["demo5@example.com"] = User("demo5@example.com", "password", "team3")
+        _mockUsers["demo6@example.com"] = User("demo6@example.com", "password", "team4")
+        _mockUsers["demo7@example.com"] = User("demo7@example.com", "password", "team4")
+        _mockUsers["demo8@example.com"] = User("demo8@example.com", "password", "team5")
+        _mockUsers["demo9@example.com"] = User("demo9@example.com", "password", "team5")
+        _mockUsers["demo10@example.com"] = User("demo10@example.com", "password", "team6")
+        _mockUsers["demo11@example.com"] = User("demo11@example.com", "password", "team6")
+        _mockUsers["demo12@example.com"] = User("demo12@example.com", "password", "team7")
+        _mockUsers["demo13@example.com"] = User("demo13@example.com", "password", "team8")
+        _mockUsers["demo14@example.com"] = User("demo14@example.com", "password", "team9")
+        _mockUsers["demo15@example.com"] = User("demo15@example.com", "password", "team10")
     }
 }
