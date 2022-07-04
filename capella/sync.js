326980f2a097e69dbc3373c6662c92d76211ffdf
Aaron LaBeau <alabeau@gmail.com>
2022-07-04 18:54:32 -0500

    updated sync function to match documentation comments

--- capella/sync.js
+++ capella/sync.js
@@ -1,34 +1,34 @@
 function sync(doc, oldDoc) {
 
 	/* Data Validation */
-	validateNotEmpty("documentType", doc.documentType);
+	validateNotEmpty("documentType", doc.documentType);  // <1>
 
 	if (doc.documentType == 'warehouse') {
 		console.log("********Processing Warehouse Docs - setting it to global/public");
 		channel('!');
 	} else {
 		console.log("********Processing Team Docs");
-		validateNotEmpty("team", doc.team);
-		if (!isDelete()) {
+		validateNotEmpty("team", doc.team); // <2>
+		if (!isDelete()) {  // <3>
 
 			/* Routing  -- add channel routing rules here for document */
-			var team = getTeam();
+			var team = getTeam();  // <4>
 			var channelId = "channel." + team;
-			console.log("********Setting Channel to " + channelId);
-			channel(channelId);
+			console.log("********Setting Channel to " + channelId);  
+			channel(channelId); // <5>
 
 			/* Authorization  - Access Control */
-			requireRole(team);
-			access("role:team1", "channel.team1");
-			access("role:team2", "channel.team2");
-			access("role:team3", "channel.team3");
-			access("role:team4", "channel.team4");
-			access("role:team5", "channel.team5");
-			access("role:team6", "channel.team6");
-			access("role:team7", "channel.team7");
-			access("role:team8", "channel.team8");
-			access("role:team9", "channel.team9");
-			access("role:team10", "channel.team10");
+			requireRole(team);  // <6>
+			access("role:team1", "channel.team1"); // <7>
+			access("role:team2", "channel.team2"); // <7>
+			access("role:team3", "channel.team3"); // <7>
+			access("role:team4", "channel.team4"); // <7>
+			access("role:team5", "channel.team5"); // <7>
+			access("role:team6", "channel.team6"); // <7>
+			access("role:team7", "channel.team7"); // <7>	
+			access("role:team8", "channel.team8"); // <7>
+			access("role:team9", "channel.team9"); // 	<7>
+			access("role:team10", "channel.team10"); // <7>
 		}
 	}
 	// get type property
